e0635950af2eda37fe5d969e7bbca50c2816dfe0
sam <145283692+samsumer01@users.noreply.github.com>
2024-09-02 09:14:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function sayHello(name){
+  console.log('Hello '+name);
+}
+// sayHello('Sam')
+
+
+// console.log(); //global
+
+// setTimeout()
+// clearInterval();
+
+// setInterval()
+// clearInterval()
+
+// var mess='';
+// global.log(global.message);
+
+// var sayHello=function(){
+
+// }
+// window.sayHello();
+
+// console.log(module);
+
+var logger = require('./logger');
+
+
+console.log('message');
